e64d93890e27bf62386b564ef74c42b35d98769b
HongyuZh <435984671@qq.com>
2023-06-08 10:46:12 +0800

    function packaged

--- watermark/main.py
+++ watermark/main.py
@@ -2,14 +2,18 @@ import numpy as np
 from PIL import Image, ImageDraw,ImageFont
 import argparse
 
+def addWatermark(image_path,text):
+    image=Image.open(image_path)
+    draw = ImageDraw.Draw(image)
+    draw.text((50,50),text)
+
+    image.save('output.png') 
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="watermarker")
     parser.add_argument('-i','--image',type=str,help="input image")
-    parser.add_argument('-w','--watermarker',type=str,help="input watermarker")
+    parser.add_argument('-w','--watermark',type=str,help="input watermark")
     args = parser.parse_args()
 
-    image=Image.open(args.image)
-    draw = ImageDraw.Draw(image)
-    draw.text((50,50),args.watermarker)
-
-    image.save('output.png')
+    addWatermark(args.image, args.watermark)
