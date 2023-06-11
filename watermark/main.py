fa217c51622a85e0bbdeecc5ef1f8dc3cffad16d
HongyuZh <435984671@qq.com>
2023-06-11 15:29:19 +0800

    fix watermark

--- watermark/main.py
+++ watermark/main.py
@@ -1,11 +1,16 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import argparse
 
 
 def addWatermark(image_path, text):
     image = Image.open(image_path)
     draw = ImageDraw.Draw(image)
-    draw.text((50, 50), text)
+    font_size = int(image.size[1] / 10)
+    font = ImageFont.truetype(
+        "/usr/share/fonts/dejavu/DejaVuSansCondensed-Bold.ttf", font_size
+    )
+    fill_color = (255, 255, 255, 128)
+    draw.text((50, 50), text, font=font, fill=fill_color)
 
     image.save("output.png")
 
