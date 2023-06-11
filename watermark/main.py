0d2318045bb9485d44a6ff166c316453508506a4
HongyuZh <435984671@qq.com>
2023-06-11 09:13:56 +0800

    fix

--- watermark/main.py
+++ watermark/main.py
@@ -1,19 +1,19 @@
-import numpy as np
-from PIL import Image, ImageDraw,ImageFont
+from PIL import Image, ImageDraw
 import argparse
 
-def addWatermark(image_path,text):
-    image=Image.open(image_path)
-    draw = ImageDraw.Draw(image)
-    draw.text((50,50),text)
 
-    image.save('output.png') 
+def addWatermark(image_path, text):
+    image = Image.open(image_path)
+    draw = ImageDraw.Draw(image)
+    draw.text((50, 50), text, font=font)
+
+    image.save("output.png")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="watermarker")
-    parser.add_argument('-i','--image',type=str,help="input image")
-    parser.add_argument('-w','--watermark',type=str,help="input watermark")
+    parser.add_argument("-i", "--image", type=str, help="input image")
+    parser.add_argument("-w", "--watermark", type=str, help="input watermark")
     args = parser.parse_args()
 
     addWatermark(args.image, args.watermark)
