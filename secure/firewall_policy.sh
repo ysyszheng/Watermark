791a41c81fee65cadc72e1b38fd9682a83afc9d3
HongyuZh <435984671@qq.com>
2023-06-13 17:47:22 +0800

    feat: iptables policy

--- secure/firewall_policy.sh
+++ secure/firewall_policy.sh
@@ -4,8 +4,12 @@ iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ALLOW
 
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp -s $1 --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
 iptables -L
